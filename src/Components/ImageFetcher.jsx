7209cdd2dc0520c8dc0718f2bbebabf1d8c34bf6
Vinay Sharma <vinaysharma511511@gmail.com>
2024-11-06 11:50:04 +0530

    add category buttons

--- src/Components/ImageFetcher.jsx
+++ src/Components/ImageFetcher.jsx
@@ -8,17 +8,24 @@ import Masonry from "react-masonry-css";
 const ImageFetcher = () => {
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("fashion");
 
   const fetchImages = async () => {
     const response = await axios.get(
-      `https://api.unsplash.com/photos/?client_id=eaI1mby7R1A_0cqvCVt41IZtxvbcRcDqbR1X1w7uJ3o&per_page=10&page=${page}`
+      `https://api.unsplash.com/search/photos/?client_id=eaI1mby7R1A_0cqvCVt41IZtxvbcRcDqbR1X1w7uJ3o&query=${query}&per_page=10&page=${page}`
     );
-    setImages((prevImages) => [...prevImages, ...response.data]);
+    setImages((prevImages) => [...prevImages, ...response.data.results]);
   };
 
   useEffect(() => {
     fetchImages();
-  }, [page]);
+  }, [page, query]);
+
+  const handleCategoryChange = (Category) => {
+    setQuery(Category);
+    setImages([]);
+    setPage(1);
+  };
 
   const breakpointColumnsObj = {
     default: 4,
@@ -28,39 +35,47 @@ const ImageFetcher = () => {
   };
 
   return (
-    <InfiniteScroll
-      dataLength={images.length}
-      next={() => setPage((prevPage) => prevPage + 1)}
-      hasMore={true}
-      loader={<h4>Loading...</h4>}
-    >
-      <Masonry
-        breakpointCols={breakpointColumnsObj}
-        className="masonry-grid"
-        columnClassName="masonry-grid-column"
+    <div>
+      <div className="category-buttons">
+        <button onClick={() => handleCategoryChange("fashion")}>Fashion</button>
+        <button onClick={() => handleCategoryChange("tech")}>Tech</button>
+        <button onClick={() => handleCategoryChange("nature")}>Nature</button>
+        <button onClick={() => handleCategoryChange("fitness")}>Fitness</button>
+      </div>
+      <InfiniteScroll
+        dataLength={images.length}
+        next={() => setPage((prevPage) => prevPage + 1)}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
       >
-        {images.map((image, index) => (
-          <div key={index}>
-            <div className="image-container">
-              <img
-                src={image.urls.small}
-                alt={image.alt_description}
-                style={{ width: "100%", borderRadius: "15px" }}
-              />
-              <button className="save-button">Save</button>
-              <div className="icon-container">
-                <div className="icon-wrapper">
-                  <UpgradeIcon className="icon" />
-                </div>
-                <div className="icon-wrapper">
-                  <MoreHorizIcon className="icon" />
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className="masonry-grid"
+          columnClassName="masonry-grid-column"
+        >
+          {images.map((image, index) => (
+            <div key={index}>
+              <div className="image-container">
+                <img
+                  src={image.urls.small}
+                  alt={image.alt_description}
+                  style={{ width: "100%", borderRadius: "15px" }}
+                />
+                <button className="save-button">Save</button>
+                <div className="icon-container">
+                  <div className="icon-wrapper">
+                    <UpgradeIcon className="icon" />
+                  </div>
+                  <div className="icon-wrapper">
+                    <MoreHorizIcon className="icon" />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-      </Masonry>
-    </InfiniteScroll>
+          ))}
+        </Masonry>
+      </InfiniteScroll>
+    </div>
   );
 };
 
